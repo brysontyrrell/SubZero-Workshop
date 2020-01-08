73182b9c136e23197e313115e21e3d27789331fd
Bryson Tyrrell <bryson.tyrrell@jamf.com>
2020-01-08 16:10:01 -0600

    Image uploads with S3 put

--- 4_Image_Uploads/src/post_image/index.py
+++ 4_Image_Uploads/src/post_image/index.py
@@ -2,14 +2,19 @@ import base64
 from cgi import parse_header
 import hashlib
 import json
+import os
 
+import boto3
 from requests_toolbelt import multipart
 
 ACCEPTED_CONTENT_TYPE = 'multipart/form-data'
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+s3_client = boto3.client('s3')
 
 
 def lambda_handler(event, context):
-    print(event)
+    client_token_claims = event['requestContext']['authorizer']['claims']
     content_type = event['headers'].get('Content-Type', '')
 
     if not content_type_check(content_type):
@@ -33,6 +38,18 @@ def lambda_handler(event, context):
 
     sha_256_hash = hashlib.sha256(multipart_binary_content).hexdigest()
 
+    s3_client.put_object(
+        Bucket=BUCKET_NAME,
+        Key=os.path.join('waiting-room', sha_256_hash),
+        Body=multipart_binary_content,
+        ContentType=multipart_content_type,
+        Metadata={
+            "UploadingClient": client_token_claims['client_id'],
+            "SHA256Hash": sha_256_hash,
+            "FileName": multipart_headers.get('filename')
+        }
+    )
+
     return {
         "isBase64Encoded": False,
         "statusCode": 200,
@@ -41,7 +58,7 @@ def lambda_handler(event, context):
                 "message": "Success!",
                 "path": event['path'],
                 "method": event['httpMethod'],
-                "token_claims": event['requestContext']['authorizer']['claims'],
+                "token_claims": client_token_claims,
                 "file": {
                     "filename": multipart_headers.get('filename'),
                     "mime-type": multipart_content_type,
