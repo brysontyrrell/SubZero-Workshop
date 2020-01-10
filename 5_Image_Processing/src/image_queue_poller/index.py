5757c942ee5cd9c991ed66fff8d891904abffe24
Bryson Tyrrell <bryson.tyrrell@jamf.com>
2020-01-09 21:46:19 -0600

    X-Ray

---
+++ 5_Image_Processing/src/image_queue_poller/index.py
@@ -0,0 +1,54 @@
+import hashlib
+import json
+import os
+import time
+
+from aws_xray_sdk.core import patch
+import boto3
+
+patch(["boto3"])
+
+STEP_FUNCTION_ARN = os.getenv("STEP_FUNCTION_ARN")
+
+s3_client = boto3.client('s3')
+sf_client = boto3.client("stepfunctions")
+
+
+def lambda_handler(event, context):
+    for record in get_s3_records(event):
+        source_bucket = record["s3"]["bucket"]["name"]
+        source_key = record["s3"]["object"]["key"]
+
+        metadata = s3_client.head_object(
+            Bucket=source_bucket,
+            Key=source_key
+        )
+
+        filename_hash = hashlib.sha1(metadata['Metadata']['filename'].encode()).hexdigest()
+
+        sf_client.start_execution(
+            stateMachineArn=STEP_FUNCTION_ARN,
+            name=f"{metadata['Metadata']['uploading-client']}-{int(time.time())}",
+            input=json.dumps(
+                dict(
+                    {
+                        'bucket': source_bucket,
+                        'key': source_key,
+                        'filaname_hash': filename_hash,
+                        'composite': f"{metadata['Metadata']['sha256-hash']}_{filename_hash}"
+                    },
+                    **metadata['Metadata']
+                )
+            )
+        )
+
+
+def get_s3_records(event):
+    for message in event["Records"]:
+        try:
+            records = json.loads(message["body"])["Records"]
+        except KeyError:
+            return list()
+
+        for record in records:
+            yield record
---
+++ 5_Image_Processing/src/post_image/index.py
@@ -0,0 +1,82 @@
+import base64
+from cgi import parse_header
+import hashlib
+import json
+import os
+
+from aws_xray_sdk.core import patch
+import boto3
+from requests_toolbelt import multipart
+
+patch(["boto3"])
+
+ACCEPTED_CONTENT_TYPE = 'multipart/form-data'
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+s3_client = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    client_token_claims = event['requestContext']['authorizer']['claims']
+    content_type = event['headers'].get('Content-Type', '')
+
+    if not content_type_check(content_type):
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 415,
+            "body": json.dumps({'message': 'Unsupported Media Type'}),
+            "headers": {
+                "Content-Type": "application/json"
+            }
+        }
+
+    raw_body = base64.b64decode(event['body'])
+    multipart_data = multipart.decoder.MultipartDecoder(raw_body, content_type)
+    # {'name': 'file', 'filename': 'filename.png'}
+    _, multipart_headers = parse_header(
+        multipart_data.parts[0].headers[b'Content-Disposition'].decode()
+    )
+    multipart_binary_content = multipart_data.parts[0].content
+    multipart_content_type = multipart_data.parts[0].headers[b'Content-Type'].decode()
+
+    sha_256_hash = hashlib.sha256(multipart_binary_content).hexdigest()
+
+    s3_client.put_object(
+        Bucket=BUCKET_NAME,
+        Key=os.path.join('waiting-room', sha_256_hash),
+        Body=multipart_binary_content,
+        ContentType=multipart_content_type,
+        Metadata={
+            "uploading-client": client_token_claims['client_id'],
+            "sha256-hash": sha_256_hash,
+            "filename": multipart_headers.get('filename')
+        }
+    )
+
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "Success!",
+                "path": event['path'],
+                "method": event['httpMethod'],
+                "token_claims": client_token_claims,
+                "file": {
+                    "filename": multipart_headers.get('filename'),
+                    "mime-type": multipart_content_type,
+                    "size": len(multipart_binary_content),
+                    "sha256": sha_256_hash
+                }
+            }
+        ),
+        "headers": {
+            "Content-Type": "application/json"
+        },
+    }
+
+
+def content_type_check(value):
+    if value.split(';')[0] == ACCEPTED_CONTENT_TYPE:
+        return True
+    return False
